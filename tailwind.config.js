ed2cfc8b4e953975ab2fc8b97fceaf20d7a92baf
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-04-14 16:40:12 +0100

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+//  @type {import('tailwindcss').Config}
+module.exports = {
+  purge: {
+    content: ["./src/**/*.html", "./src/**/*.js"],
+    options: {
+      safelist: [],
+    },
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
